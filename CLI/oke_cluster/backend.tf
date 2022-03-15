d658be5032711c0fadde0964b12f1996277230ea
nicolinap <nikolinapieri@gmail.com>
2022-03-15 15:19:42 +0000

    Updated backend bucket

--- CLI/oke_cluster/backend.tf
+++ CLI/oke_cluster/backend.tf
@@ -7,10 +7,10 @@
 
 terraform {
   backend "s3" {
-    bucket   = "npier-nested-bucket-20220309-0801"
-    key      = "Samples/02.tfstate"
-    region   = "uk-london-1"
-    endpoint = "https://idhkis4m3p5e.compat.objectstorage.uk-london-1.oraclecloud.com"
+    bucket   = "tfstate_bucket"
+    key      = "Samples/oke_cluster.tfstate"
+    region   = "re-region-1"
+    endpoint = "https://Tenancy_ObjectStorage_namespace.compat.objectstorage.re-region-1.oraclecloud.com"
 
     skip_region_validation      = true
     skip_credentials_validation = true
