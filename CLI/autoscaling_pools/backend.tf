aaec8b163468d5eff643b6083ce291b6283c0865
nicolinap <nikolinapieri@gmail.com>
2022-03-17 12:42:04 +0000

    Updated backends

--- CLI/autoscaling_pools/backend.tf
+++ CLI/autoscaling_pools/backend.tf
@@ -5,26 +5,12 @@
 # Purpose:Handles the storage of tfstate file in backend bucket
 
 
-/*terraform {
-  backend "s3" {
-    bucket   = "tfstate_bucket"
-    key      = "Samples/oke_cluster.tfstate"
-    region   = "re-region-1"
-    endpoint = "https://Tenancy_ObjectStorage_namespace.compat.objectstorage.re-region-1.oraclecloud.com"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-  }
-}*/
-
 terraform {
   backend "s3" {
-    bucket   = "npier-nested-bucket-20220309-0801"
-    key      = "Samples/auto1.tfstate"
-    region   = "uk-london-1"
-    endpoint = "https://idhkis4m3p5e.compat.objectstorage.uk-london-1.oraclecloud.com"
+    bucket   = "tfstate_bucket"
+    key      = "Samples/autoscaling_pool.tfstate"
+    region   = "re-region-1"
+    endpoint = "https://Tenancy_ObjectStorage_namespace.compat.objectstorage.re-region-1.oraclecloud.com"
 
     skip_region_validation      = true
     skip_credentials_validation = true
--- CLI/sample_load_balancers/sample_instance_pool_load_balancer/backend.tf
+++ CLI/sample_load_balancers/sample_instance_pool_load_balancer/backend.tf
@@ -5,26 +5,12 @@
 # Purpose:Handles the storage of tfstate file in backend bucket
 
 
-/*terraform {
+terraform {
   backend "s3" {
     bucket   = "tfstate_bucket"
     key      = "Samples/sample_instance_pool_load_balancer.tfstate"
     region   = "re-region-1"
     endpoint = "https://Tenancy_ObjectStorage_namespace.compat.objectstorage.re-region-1.oraclecloud.com"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-  }
-}*/
-
-terraform {
-  backend "s3" {
-    bucket   = "npier-nested-bucket-20220309-0801"
-    key      = "Samples/22.tfstate"
-    region   = "uk-london-1"
-    endpoint = "https://idhkis4m3p5e.compat.objectstorage.uk-london-1.oraclecloud.com"
 
     skip_region_validation      = true
     skip_credentials_validation = true
